2d528b3f8969d343a8740a765d0da89d5b7d7120
Adriano Flach <adriano.araujo@philips.com>
2020-08-04 19:00:28 -0300

    card

--- creative-cards/src/App.vue
+++ creative-cards/src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
     <nav-header></nav-header>
+    <card-front></card-front>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import CardFront from './components/card/CardFront.vue'
 
 export default {
   name: 'App',
   components: {
-    navHeader: Header
+    navHeader: Header,
+    cardFront: CardFront,
   }
 }
 </script>
 
 <style>
+  body {
+    font-family: Verdana, Geneva, sans-serif;
+    color: #333
+  }
 </style>
